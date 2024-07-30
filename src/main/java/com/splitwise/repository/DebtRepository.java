ec635fd6449daa5faacaab91e89153a2fddd2a0d
Avibtb <avi.singh.iit01@gmail.com>
2024-07-31 01:33:51 +0530

    Added ExpenseRepository and fixed DebtRepository .

--- src/main/java/com/splitwise/repository/DebtRepository.java
+++ src/main/java/com/splitwise/repository/DebtRepository.java
@@ -4,10 +4,12 @@ import com.splitwise.entity.Debt;
 import com.splitwise.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface DebtRepository extends JpaRepository<Debt,String> {
 
     Optional<Debt> findByPayerAndPayee(Users payer,Users payee);
---
+++ src/main/java/com/splitwise/repository/ExpenseRepository.java
@@ -0,0 +1,14 @@
+package com.splitwise.repository;
+
+import com.splitwise.entity.Expense;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense,String> {
+
+    List<Expense> findAllByPayerId(String payerId, Pageable pageable);
+}
