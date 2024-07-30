1260a268f1cf20a59e31eaf2abd31e3733605564
Avibtb <avi.singh.iit01@gmail.com>
2024-07-31 00:49:04 +0530

    Created Factory classes.

---
+++ src/main/java/com/splitwise/factory/AmountSplitterFactory.java
@@ -0,0 +1,34 @@
+package com.splitwise.factory;
+
+import com.splitwise.enums.ExpenseType;
+import com.splitwise.splitter.*;
+import com.splitwise.exception.InvalidAmountSplitterException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AmountSplitterFactory {
+
+    @Autowired
+    private ExactAmountSplitter exactAmountSplitter;
+
+    @Autowired
+    private EqualAmountSplitter equalAmountSplitter;
+
+    @Autowired
+    private PercentAmountSplitter percentAmountSplitter;
+
+    @Autowired
+    private ShareAmountSplitter shareAmountSplitter;
+
+    public AmountSplitter getObject(ExpenseType expenseType){
+       switch (expenseType){
+           case EXACT -> {return exactAmountSplitter;}
+           case EQUAL -> {return equalAmountSplitter;}
+           case PERCENT -> {return percentAmountSplitter;}
+           case SHARE -> {return shareAmountSplitter;}
+           default -> throw new InvalidAmountSplitterException();
+       }
+
+    }
+}
---
+++ src/main/java/com/splitwise/factory/ExpenseValidatorFactory.java
@@ -0,0 +1,47 @@
+package com.splitwise.factory;
+
+import com.splitwise.enums.ExpenseType;
+import com.splitwise.exception.InvalidExpenseTypeException;
+import com.splitwise.validator.expense.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExpenseValidatorFactory {
+
+    @Autowired
+    private ExactExpenseValidator exactExpenseValidator;
+
+    @Autowired
+    private PercentExpenseValidator percentExpenseValidator;
+
+    @Autowired
+    private ShareExpensValidator shareExpensValidator;
+
+    @Autowired
+    private IndividualExpenseValidator individualExpenseValidator;
+
+    @Autowired
+    private EqualExpenseValidator equalExpenseValidator;
+
+    public ExpenseValidator getInstance(ExpenseType expenseType){
+        switch (expenseType){
+            case EXACT -> {
+                return exactExpenseValidator;
+            }
+            case SHARE -> {
+                return shareExpensValidator;
+            }
+            case PERCENT -> {
+                return percentExpenseValidator;
+            }
+            case EQUAL -> {
+                return equalExpenseValidator;
+            }
+            case INDIVIDUAL -> {
+                return individualExpenseValidator;
+            }
+            default -> throw new InvalidExpenseTypeException(expenseType);
+        }
+    }
+}
