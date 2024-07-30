653fed83b67a29de1876b1170e496ac7d024ae97
Avibtb <avi.singh.iit01@gmail.com>
2024-07-31 00:24:31 +0530

    Created more custom exception class.

---
+++ src/main/java/com/splitwise/exception/UserIsAlreadyExistsInGroupException.java
@@ -0,0 +1,10 @@
+package com.splitwise.exception;
+
+public class UserIsAlreadyExistsInGroupException extends RuntimeException{
+    public UserIsAlreadyExistsInGroupException(String message){
+        super(message);
+    }
+    public UserIsAlreadyExistsInGroupException(){
+        super("User is Already Exists in the group");
+    }
+}
