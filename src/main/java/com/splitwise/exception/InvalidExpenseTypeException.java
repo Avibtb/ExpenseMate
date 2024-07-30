3385fc1dfe0ef0b774f725a8a8c159212d3807e1
Avibtb <avi.singh.iit01@gmail.com>
2024-07-31 00:24:17 +0530

    Created more custom exception class.

---
+++ src/main/java/com/splitwise/exception/InvalidExpenseTypeException.java
@@ -0,0 +1,13 @@
+package com.splitwise.exception;
+
+
+import com.splitwise.enums.ExpenseType;
+
+public class InvalidExpenseTypeException extends RuntimeException{
+    public InvalidExpenseTypeException(){
+        super("Invalid Expense type");
+    }
+    public InvalidExpenseTypeException(ExpenseType expenseType){
+        super("Invalid Expense type "+ expenseType);
+    }
+}
